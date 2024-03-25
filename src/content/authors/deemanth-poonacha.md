f898ab12c0a622797b9c5760f703e7ebb41ef391
Deemanth Poonacha <deemanth.poonacha@ikara.ai>
2024-03-25 17:06:48 +0530

    Contents and components updated

---
+++ src/content/authors/deemanth-poonacha.md
@@ -0,0 +1,4 @@
+---
+name: Dwight Schrute
+image: ../posts/images/bloggy-logo.jpg
+---
---
+++ src/content/config.ts
@@ -0,0 +1,36 @@
+import { z, defineCollection, type CollectionEntry } from "astro:content";
+// import {format} from "date-fns"
+
+const authorsCollection = defineCollection({
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      image: image(),
+    }),
+});
+
+const postsCollection = defineCollection({
+  schema: ({ image }) =>
+    z.object({
+      author: z.string(),
+      categories: z.array(z.string()),
+      date: z.string().transform((str) =>
+        new Date(str).toLocaleDateString("en-GB", {
+          weekday: "short",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        }),
+      ),
+      featured: z.boolean().default(false),
+      image: image(),
+      title: z.string(),
+    }),
+});
+
+export const collections = {
+  authors: authorsCollection,
+  posts: postsCollection,
+};
+
+export type Post = CollectionEntry<"posts">;

398b9921d72ede2f42a56c79423a0cfc0143b6ad
deemanth <deemanth.poonacha@ikara.ai>
2025-01-21 11:45:03 +0530

    Content collection updated

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly POSTS_PER_PAGE: number;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

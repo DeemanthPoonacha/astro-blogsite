8f7636e4ea3060ef70917f83a5bd651849da71b1
deemanth <deemanth.poonacha@ikara.ai>
2025-02-03 19:06:09 +0530

    Home page design updated and CTA added

--- src/components/ui/HeroBanner.tsx
+++ src/components/ui/HeroBanner.tsx
@@ -25,13 +25,17 @@ const HeroBanner = () => {
   }, []);
 
   const { scrollYProgress } = useScroll({});
-  const zIndex = useSpring(
-    useTransform(scrollYProgress, [0, 0.2], [10, 50]),
-    springConfig,
-  );
+  // const zIndex = useSpring(
+  //   useTransform(scrollYProgress, [0, 0.2], [10, -5990]),
+  //   springConfig,
+  // );
 
   const scale = useSpring(
     useTransform(scrollYProgress, [0.3, 0.6], [1, 2.77]),
+    springConfig,
+  );
+  const ctaMt = useSpring(
+    useTransform(scrollYProgress, [0.3, 0.6], [12, 150]),
     springConfig,
   );
 
@@ -43,7 +47,7 @@ const HeroBanner = () => {
     useTransform(
       scrollYProgress,
       [0, 0.2, 0.6, 0.7],
-      [0, -diff[1], -diff[1], -6 * diff[1]],
+      [0, -diff[1], -diff[1], -17 * diff[1]],
     ),
     springConfig,
   );
@@ -66,48 +70,77 @@ const HeroBanner = () => {
     useTransform(scrollYProgress, [0.1, 0.3], [0, -diff[0] + xOffset]),
     springConfig,
   );
-  const yOffset = +diff[2] > 56 ? 14 : 20;
+  const yOffset = diff[2] > 56 ? 14 : 20;
   const translateYTitle = useSpring(
     useTransform(scrollYProgress, [0, 0.2], [0, -diff[1] - 360 + yOffset]),
     springConfig,
   );
 
   return (
-    <div className="h-96 pointer-events-none max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full flex flex-col items-center ">
+    <div className="max-w-7xl relative mx-auto w-full flex flex-col items-center min-h-svh">
       <motion.img
         id="mainLogo"
         style={{
           width,
           translateX,
           translateY,
-          zIndex,
+          // zIndex,
         }}
         src="/images/logo.png"
         alt="Bloggy logo"
         width={250}
         height={250}
-        className="z-50 fixed top-1/4 text-center rounded-xl invert dark:invert-0"
+        className="z-50 pointer-events-none fixed top-1/4 text-center rounded-xl invert dark:invert-0 -mt-20"
       />
       <motion.p
         style={{
           translateX: translateXTitle,
           translateY: translateYTitle,
-          zIndex,
+          // zIndex,
         }}
-        className="z-50 fixed top-[calc(25%+360px)] uppercase prose font-blod text-3xl sm:text-6xl text-zinc-900 dark:text-zinc-100"
+        className="z-50 pointer-events-none fixed top-[calc(25%+360px)] uppercase prose font-blod text-3xl sm:text-6xl text-zinc-900 dark:text-zinc-100 -mt-20"
       >
         Bloggy
       </motion.p>
       <motion.div
-        className="fixed top-[calc(25%+420px)] -z-10"
-        style={{ width: textWidth, translateY: translateYSubTitle }}
+        className="fixed top-[calc(25%+420px)] flex flex-col items-center gap-2 justify-between -mt-20"
+        style={{
+          width: textWidth,
+          // height: translateYSubTitle,
+          translateY: translateYSubTitle,
+        }}
       >
         <motion.p
           style={{ scale }}
-          className="-z-50 p-2 pb-20 drop-shadow-xl max-w-2xl text-xl max-md:text-2xl text-center py-2 mt-1/2 dark:text-neutral-200"
+          className="-z-50 pointer-events-none p-2 drop-shadow-xl max-w-2xl text-xl max-md:text-2xl text-center py-2 mt-1/2 prose dark:prose-invert"
         >
           Your ultimate destination for insightful articles
         </motion.p>
+
+        <motion.a
+          style={{ marginTop: ctaMt }}
+          className="z-50 w-64 prose prose-xl bg-primary text-primary-foreground rounded-full p-4 text-center cursor-pointer font-bold px-5 py-2.5 overflow-hidden group bg-cyan-500 relative hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-cyan-400 transition-all ease-out duration-300"
+          href="/dashboard"
+        >
+          <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+            <svg
+              className="w-5 h-5 mb-1"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              ></path>
+            </svg>
+          </span>
+          <span className="absolute right-0 w-8 h-32 -mt-20 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+          Let's get started!
+        </motion.a>
       </motion.div>
     </div>
   );
--- src/components/ui/HeroParallax.tsx
+++ src/components/ui/HeroParallax.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { motion, useScroll, useTransform, useSpring } from "framer-motion";
 import InfiniteMovingCards from "./InfiniteMovingCards";
 import HeroBanner from "./HeroBanner";
@@ -15,7 +15,7 @@ const HeroParallax = ({
   const firstRow = items.slice(0, 5);
   const secondRow = items.slice(5, 10);
   const thirdRow = items.slice(10, 15);
-  const ref = useRef(null);
+  const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["start start", "end start"],
@@ -38,12 +38,27 @@ const HeroParallax = ({
     useTransform(scrollYProgress, [0, 0.2], [-700, 500]),
     springConfig,
   );
+  const [refHeight, setRefHeight] = useState(
+    ref.current?.getBoundingClientRect().height || 1200,
+  );
+  console.log("🚀 ~ refHeight:", refHeight);
+  useEffect(() => {
+    console.log("🚀 ~ window:", window.innerHeight);
+
+    if (ref.current)
+      setRefHeight(
+        (ref.current.getBoundingClientRect().height / window.innerHeight) *
+          1000,
+      );
+  }, [ref.current]);
+
   return (
     <div
       ref={ref}
-      className="min-h-[350svh] -mt-5 overflow-hidden  antialiased relative flex flex-col self-auto l[perspective:1000px] l[transform-style:preserve-3d]"
+      // className="min-h-[380svh] [@media(max-height:953px)]:min-h-[500svh] [@media(max-height:953px)]:mb-[100svh]  [@media(max-height:800px)]:min-h-[600svh] mb-12 [@media(max-height:800px)]:mb-[240svh] -mt-8 overflow-hidden  antialiased relative flex flex-col self-auto l[perspective:1000px] l[transform-style:preserve-3d]"
+      className="-mt-8 overflow-hidden antialiased relative flex flex-col self-auto"
+      style={{ marginBottom: refHeight + 56 }}
     >
-      <HeroBanner />
       <motion.div
         style={{
           rotateX,
@@ -51,7 +66,7 @@ const HeroParallax = ({
           translateY,
           opacity,
         }}
-        className="mt-40"
+        className="mt-[50%]"
       >
         <motion.div className="flex flex-row-reverse space-x-reverse space-x-20">
           <InfiniteMovingCards items={firstRow} speed="slow" />
@@ -67,6 +82,7 @@ const HeroParallax = ({
           <InfiniteMovingCards items={thirdRow} speed="slow" />
         </motion.div>
       </motion.div>
+      <HeroBanner />
     </div>
   );
 };
